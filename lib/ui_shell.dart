b0130d4277dece2771416692c276545acdc1ae1d
agsbzhxndjdj <blalnwaf0@gmail.com>
2026-08-13 13:12:27 +0300

    Implement HomeShell widget with bottom navigation

---
+++ lib/ui_shell.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'ui_home.dart';
+import 'ui_pages.dart';
+import 'core.dart';
+
+class HomeShell extends StatelessWidget {
+  const HomeShell({super.key});
+  @override
+  Widget build(BuildContext context) => ValueListenableBuilder<int>(
+      valueListenable: App.tab,
+      builder: (ctx, tab, _) => Scaffold(
+            body: const IndexedStack(children: [
+              HomePage(), HistoryPage(), DownloadsPage(), ChannelsPage()
+            ]),
+            bottomNavigationBar: NavigationBar(
+              selectedIndex: tab,
+              onDestinationSelected: (i) => App.tab.value = i,
+              destinations: const [
+                NavigationDestination(icon: Icon(Icons.movie_outlined), selectedIcon: Icon(Icons.movie), label: 'الأفلام'),
+                NavigationDestination(icon: Icon(Icons.history_outlined), selectedIcon: Icon(Icons.history), label: 'شاهدتها'),
+                NavigationDestination(icon: Icon(Icons.download_outlined), selectedIcon: Icon(Icons.download), label: 'تحميلاتي'),
+                NavigationDestination(icon: Icon(Icons.rss_feed_outlined), selectedIcon: Icon(Icons.rss_feed), label: 'القنوات'),
+              ],
+            ),
+          ));
+}
