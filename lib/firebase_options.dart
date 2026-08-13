61d1541a71602ce26caadeb391d331a73e04a417
agsbzhxndjdj <blalnwaf0@gmail.com>
2026-08-13 05:19:51 +0300

    Create FirebaseOptions class for Firebase setup
    Added FirebaseOptions class for Firebase configuration.

---
+++ lib/firebase_options.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class Fb {
+  static const options = FirebaseOptions(
+    apiKey: 'AIzaSyDThT37CJIj5XAEz-97MUhqWzG9qDO298k',
+    appId: '1:1028373813571:android:d2f8cb3d5232285dcdb7f6',
+    messagingSenderId: '1028373813571',
+    projectId: 'tele-cinema',
+    storageBucket: 'tele-cinema.firebasestorage.app',
+    authDomain: 'tele-cinema.firebasestorage.app',
+  );
+}
