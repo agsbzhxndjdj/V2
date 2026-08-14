dbfb7f9f73602cefc7821442255c1139e71ce817
agsbzhxndjdj <blalnwaf0@gmail.com>
2026-08-14 04:22:53 +0300

    Add async import to ui.dart

--- lib/ui.dart
+++ lib/ui.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:cached_network_image/cached_network_image.dart';
