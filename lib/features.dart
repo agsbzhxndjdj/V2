3aa5917c5370a7c5f8c68f707ab43ff70c7d58ec
agsbzhxndjdj <blalnwaf0@gmail.com>
2026-08-15 14:41:21 +0300

    Add import for features2.dart

--- lib/features.dart
+++ lib/features.dart
@@ -6,6 +6,7 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'features2.dart';
 import 'package:path_provider/path_provider.dart';
 import 'core.dart';
 import 'lang.dart';
