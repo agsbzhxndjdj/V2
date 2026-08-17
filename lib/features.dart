15f7e115933dff87d02586bff310e3a16312c432
agsbzhxndjdj <blalnwaf0@gmail.com>
2026-08-17 06:49:07 +0300

    Add import for rendering and MovieCard

--- lib/features.dart
+++ lib/features.dart
@@ -8,7 +8,8 @@ import 'package:dio/dio.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
-
+import 'package:flutter/rendering.dart';
+import 'ui.dart' show MovieCard;
 import 'core.dart';
 import 'lang.dart';
 import 'tv.dart';
